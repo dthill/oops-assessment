31b2c39803c5d7388477bfa3e9663457c59ece43
d <dthill@gmx.net>
2022-04-23 13:30:21 +0200

    implement submenu

--- src/main/java/menu/MainMenuFactory.java
+++ src/main/java/menu/MainMenuFactory.java
@@ -4,23 +4,25 @@ import commands.DeleteCommand;
 import commands.ReadCommand;
 import commands.SearchCommand;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MainMenuFactory {
     public static Menu createMainMenu() {
         Menu menu = new Menu();
         menu.addHeader("App name")
                 .addTitle("Read Files")
                 .addMenuOption(createReadOption())
-                .addParent(menu)
+                .addMenuOption(createEditAndSearchOption(menu))
                 .addExitOption();
         return menu;
     }
 
 
-    private static Menu createEditAndSearchOption() {
-        return null;
+    private static Menu createEditAndSearchOption(Menu parent) {
+        return new Menu()
+                .addTitle("Edit and Search Submenu")
+                .addMenuOption(createDeleteOption())
+                .addMenuOption(createSearchOption())
+                .addParent(parent)
+                .addExitOption();
     }
 
     private static Menu createReadOption() {
@@ -28,11 +30,11 @@ public class MainMenuFactory {
     }
 
     private static Menu createDeleteOption() {
-        return new Menu();
+        return new Menu().addCommand(new DeleteCommand()).addTitle("Delete File");
     }
 
     private static Menu createSearchOption() {
-        return new Menu();
+        return new Menu().addCommand(new SearchCommand()).addTitle("Search File");
     }
 
 }
